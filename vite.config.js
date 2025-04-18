e0dc5861bc195010bc60b30307d0845cefbfdbc3
ivant <ivan-andretti.tefang@efrei.net>
2025-04-18 03:02:53 +0200

    Mise à jour cybersécurité

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/PORTFOLIO/", // NOM EXACT du repo GitHub
+  base: "/PORTFOLIO/", 
 })
 
